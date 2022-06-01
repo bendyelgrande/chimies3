7cdc734b72c7ed25ea02d4e39e3d11d7dd3e1543
bendyelgrande <106599654+bendyelgrande@users.noreply.github.com>
2022-06-01 08:38:34 -0400

    Add files via upload

---
+++ chimie.js
@@ -0,0 +1,200 @@
+function hide(){
+    var x=document.getElementById('men')
+    if(x.style.visibility==='hidden'){
+    x.style.visibility= 'visible';
+    }
+    else {
+       x.style.visibility= 'hidden';
+    }
+}
+function nsa(){
+    window.scrollTo({ top: 620, behavior: 'smooth' });
+}
+function f1(){
+    window.scrollTo({ top: 1180, behavior: 'smooth' });
+}
+function con(){
+    window.scrollTo({ top: 1180, behavior: 'smooth' });
+}
+function f2(){
+    window.scrollTo({ top: 1745, behavior: 'smooth' });
+}
+function cace(){
+    window.scrollTo({ top: 1745, behavior: 'smooth' });
+}
+function cdm(){
+    window.scrollTo({ top: 2320, behavior: 'smooth' });
+}
+function f3(){
+    window.scrollTo({ top: 2320, behavior: 'smooth' });
+}
+function f4(){
+    window.scrollTo({ top: 2920, behavior: 'smooth' });
+}
+function pgs(){
+    window.scrollTo({ top: 2920, behavior: 'smooth' });
+}
+
+
+function b1(){
+    window.scrollTo({ top: 610, behavior: 'smooth' });
+}
+function b2(){
+    window.scrollTo({ top: 1175, behavior: 'smooth' });
+}
+function b3(){
+    window.scrollTo({ top: 1740, behavior: 'smooth' });
+}
+function b4(){
+    window.scrollTo({ top: 2305, behavior: 'smooth' });
+}
+function b5(){
+    window.scrollTo({ top: 2870, behavior: 'smooth' });
+}
+function b6(){
+    window.scrollTo({ top: 3470, behavior: 'smooth' });
+}
+function b7(){
+    window.scrollTo({ top: 4030, behavior: 'smooth' });
+}
+function b8(){
+    window.scrollTo({ top: 4600, behavior: 'smooth' });
+}
+function b16(){
+    window.scrollTo({ top: 4030, behavior: 'smooth' });
+}
+function b15(){
+    window.scrollTo({ top: 3470, behavior: 'smooth' });
+}
+function b14(){
+    window.scrollTo({ top: 2870, behavior: 'smooth' });
+}
+function b13(){
+    window.scrollTo({ top: 2305, behavior: 'smooth' });
+}
+function b12(){
+    window.scrollTo({ top: 1740, behavior: 'smooth' });
+}
+function b11(){
+    window.scrollTo({ top: 1175, behavior: 'smooth' });
+}
+function b10(){
+    window.scrollTo({ top: 610, behavior: 'smooth' });
+}
+function b9(){
+    window.scrollTo({ top: 10, behavior: 'smooth' });
+}
+ function fdl(){
+    var x=document.getElementById('fichest')
+    if(x.style.display='none'){
+      x.style.display='block';
+ }
+ var y=document.getElementById('nsa')
+ if(y.style.display='block'){
+   y.style.display='none';
+}
+ var y=document.getElementById('cdm')
+ if(y.style.display='block'){
+   y.style.display='none';
+}
+ var y=document.getElementById('cace')
+ if(y.style.display='block'){
+   y.style.display='none';
+}
+ var y=document.getElementById('con')
+ if(y.style.display='block'){
+   y.style.display='none';
+}
+ var y=document.getElementById('pgs')
+ if(y.style.display='block'){
+   y.style.display='none';
+}
+ var x=document.getElementById('men')
+    if(x.style.visibility='visible'){
+    x.style.visibility='hidden';
+    }
+}
+function bi(){
+    var x=document.getElementById('fichest')
+    if(x.style.display='block'){
+    x.style.display='none';
+    }
+    var x=document.getElementById('cace')
+    if(x.style.display='none'){
+    x.style.display='block';
+    }
+    var x=document.getElementById('con')
+    if(x.style.display='none'){
+    x.style.display='block';
+    }
+    var x=document.getElementById('cdm')
+    if(x.style.display='none'){
+    x.style.display='block';
+    }
+    var x=document.getElementById('pgs')
+    if(x.style.display='none'){
+    x.style.display='block';
+    }
+    var x=document.getElementById('nsa')
+    if(x.style.display='none'){
+    x.style.display='block';
+    }
+   
+}
+function pict(){
+    var x=document.getElementById('pic')
+    if(x.style.display='none'){
+    x.style.display='block';
+    }  
+    var x=document.getElementById('nsa')
+    if(x.style.display='block'){
+    x.style.display='none';
+    } 
+    var x=document.getElementById('con')
+    if(x.style.display='block'){
+    x.style.display='none';
+    } 
+    var x=document.getElementById('cdm')
+    if(x.style.display='block'){
+    x.style.display='none';
+    } 
+    var x=document.getElementById('cace')
+    if(x.style.display='block'){
+    x.style.display='none';
+    } 
+    var x=document.getElementById('pgs')
+    if(x.style.display='block'){
+    x.style.display='none';
+    } 
+    var x=document.getElementById('men')
+    if(x.style.visibility='visible'){
+    x.style.visibility='hidden';
+    } 
+}
+function bi1(){
+    
+    var x=document.getElementById('pic')
+    if(x.style.display='block'){
+    x.style.display='none';
+    }
+    var x=document.getElementById('cace')
+    if(x.style.display='none'){
+    x.style.display='block';
+    }
+    var x=document.getElementById('con')
+    if(x.style.display='none'){
+    x.style.display='block';
+    }
+    var x=document.getElementById('cdm')
+    if(x.style.display='none'){
+    x.style.display='block';
+    }
+    var x=document.getElementById('pgs')
+    if(x.style.display='none'){
+    x.style.display='block';
+    }
+    var x=document.getElementById('nsa')
+    if(x.style.display='none'){
+    x.style.display='block';
+    }
+}
